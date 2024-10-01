781b7bb0b03da55191f778d728a4f41407395b5b
Robig <Robig@MSIVECTOR17_UNI>
2024-10-01 15:22:10 +0200

    Agregar archivos de proyecto.

---
+++ Prueba_CMake.cpp
@@ -0,0 +1,12 @@
+﻿// Prueba_CMake.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Prueba_CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
