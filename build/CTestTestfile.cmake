4b4db76637e981a1c8d928578c4504eca3cd3a14
Roberto Iglesia Girbal <robertoiglesiagirbal@gmail.com>
2024-12-18 13:29:14 +0100

    Recompilación de comprobación

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(CompiladorCorrecto "C:/Program Files/CMake/bin/cmake.exe" "-E" "echo" "Compilador: GNU, Versión: 13.2.0")
-set_tests_properties(CompiladorCorrecto PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Robig/Desktop/MSMK/Segundo Curso/Primer Cuatrimestre/UNIT 20 - APPLIED PROGRAMMING & DESIGN PRINCIPLES/Intermedia-3/CMakeLists.txt;55;add_test;C:/Users/Robig/Desktop/MSMK/Segundo Curso/Primer Cuatrimestre/UNIT 20 - APPLIED PROGRAMMING & DESIGN PRINCIPLES/Intermedia-3/CMakeLists.txt;0;")
+set_tests_properties(CompiladorCorrecto PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Robig/Desktop/MSMK/Segundo Curso/Primer Cuatrimestre/UNIT 20 - APPLIED PROGRAMMING & DESIGN PRINCIPLES/Intermedia-3/CMakeLists.txt;56;add_test;C:/Users/Robig/Desktop/MSMK/Segundo Curso/Primer Cuatrimestre/UNIT 20 - APPLIED PROGRAMMING & DESIGN PRINCIPLES/Intermedia-3/CMakeLists.txt;0;")
